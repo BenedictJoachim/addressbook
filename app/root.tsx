3b0c3a088dc067b2e36bcc71f33426bd68e75ce8
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-12-14 15:43:51 +0000

    Added HydrateFallback export to show up before the app is hydrated(rendering on the client for the first time)

--- app/root.tsx
+++ app/root.tsx
@@ -126,3 +126,12 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   );
 }
 
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
+  );
+}
+
