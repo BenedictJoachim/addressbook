b114b9f0e7e6613abe461991ba81c1ea7aee7b7c
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-12-15 20:00:46 +0000

    Finalizing the UI with optmistic values, managing history stack, adding a search spinner, forms without navigation, submitting forms on Change, sychronizing URLs to Form state, and the cancel button navigation on the edit-contact route

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,6 @@
-import { Form } from "react-router";
+import { Form, useFetcher } from "react-router";
 
-import { getContact, type ContactRecord } from "../data";
+import { getContact, updateContact, type ContactRecord } from "../data";
 import type { Route } from "./+types/contact";
 
 export async function loader({ params }: Route.LoaderArgs){
@@ -11,6 +11,10 @@ export async function loader({ params }: Route.LoaderArgs){
   return { contact };
 }
 
+export async function action({ params, request}: Route.ActionArgs){
+  const formData = await request.formData();
+  return updateContact(params.contactId, {favorite: formData.get("favorite") ==="true",})
+}
 
 export default function Contact({ loaderData }: Route.ComponentProps) {
   const { contact } = loaderData;
@@ -79,10 +83,13 @@ function Favorite({
 }: {
   contact: Pick<ContactRecord, "favorite">;
 }) {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
 
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         aria-label={
           favorite
@@ -94,6 +101,6 @@ function Favorite({
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
