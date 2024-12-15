8c541e0160bfd57fb0bac0714d53211e06e5e765
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-12-15 10:10:04 +0000

    Redirecting new records to the edit page by returning a redirect on the root action function

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  isRouteErrorResponse
+  isRouteErrorResponse,
+  redirect
 } from "react-router";
 import type { Route } from "./+types/root";
 
@@ -11,7 +12,7 @@ import { createEmptyContact } from "./data";
 
 export async function action() {
   const contact = await createEmptyContact();
-  return { contact };
+  return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function App({ loaderData }: Route.ComponentProps) {
